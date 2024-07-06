6398c6998d41648775752f222d3ae8aed2c6c929
eum3l <77971322+eum3l@users.noreply.github.com>
2024-07-06 18:01:30 +0200

    feat(nixos): add user, group option

--- nixos.nix
+++ nixos.nix
@@ -253,8 +253,8 @@ in
                         let
                           userDefaultPerms = {
                             inherit (defaultPerms) mode;
-                            user = name;
-                            group = users.${userDefaultPerms.user}.group;
+                            user = config.user or name;
+                            group = config.group or users.${userDefaultPerms.user}.group;
                           };
                           fileConfig =
                             { config, ... }:
@@ -307,6 +307,22 @@ in
                                   Cannot currently be automatically
                                   deduced due to a limitation in
                                   nixpkgs.
+                                '';
+                              };
+
+                              user = mkOption {
+                                type = str;
+                                default = name;
+                                description = ''
+                                  The user to give ownership of the files.
+                                '';
+                              };
+
+                              group = mkOption {
+                                type = str;
+                                default = users.${name}.group;
+                                description = ''
+                                  The group to give ownership of the files.
                                 '';
                               };
 
@@ -577,7 +593,7 @@ in
                       home = null;
                       mode = "0700";
                       user = dir.user;
-                      group = users.${dir.user}.group;
+                      group = dir.group;
                       inherit defaultPerms;
                       inherit (dir) persistentStoragePath enableDebugging;
                     };
